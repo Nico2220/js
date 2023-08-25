96a12c6f61ff81f7a71efd7e61edbc5824529907
Nicolas Philibert <phil@Nicolass-MacBook-Pro.local>
2023-08-25 16:56:58 +0300

    add promistify

---
+++ promistify.js
@@ -0,0 +1,30 @@
+function promisify(callback) {
+  return function (...args) {
+    return new Promise((resolve, reject) => {
+      function handleErrorAndValue(error, value) {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(value);
+        }
+      }
+      callback(...args, handleErrorAndValue);
+    });
+  };
+}
+
+function adder(x, y, handleErrorAndValue) {
+  const value = x + y;
+  if (typeof value !== "number") {
+    const error = new Error("Not a number");
+    handleErrorAndValue(error, null);
+  } else {
+    handleErrorAndValue(null, value);
+  }
+}
+
+const promisifyAdder = promisify(adder);
+
+promisifyAdder(1, "1")
+  .then((v) => console.log(v))
+  .catch((err) => console.log(err));

e83c7206c2180b1ddd1affb8b61e773e43d008e7
Nicolas Philibert <phil@Nicolass-MacBook-Pro.local>
2023-11-26 08:38:30 +0300

    add panlidrom checker

---
+++ algo-test/indexEqualValue.js
@@ -0,0 +1,9 @@
+// function indexEqualValue(array) {
+//   for (let i = 0; i < array.length; i++) {
+//     if (i === array[i]) return i;
+//   }
+
+//   return -1;
+// }
+
+console.log(indexEqualValue([-5, -3, 0, 3, 4, 5, 9]));

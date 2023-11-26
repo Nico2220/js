7cafaa05f129b83269e4b5ae314eab1262c2145b
Nicolas Philibert <phil@Nicolass-MacBook-Pro.local>
2023-11-26 08:37:55 +0300

    add panlidrom checker

---
+++ algo-test/string/isPanlidrom.js
@@ -0,0 +1,15 @@
+function isPanlidrom(str) {
+  let left = 0;
+  let right = str.length - 1;
+
+  while (left < right) {
+    if (str[left] !== str[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
+}
+
+console.log(isPanlidrom("abcdcb"));
